28ba65c61acc469c5fbe06d055e625a1b113a9b0
Marvin Kome <marvinkome@gmail.com>
2017-12-18 15:53:30 +0100

    Second Commit - Moved the result to a dictionary or state(key) and lga(value)

--- spider.py
+++ spider.py
@@ -7,14 +7,25 @@ def main():
     html = urlopen(url)
     bs_obj = BeautifulSoup(html, 'html.parser')
 
+    states = set()
+    d = {}
     table = bs_obj.find('table', attrs={'class':'wikitable'})
     rows = table.find_all('tr')
 
     for row in rows:
-        for col in row.find_all(['td','th']):
-            print(col.get_text())
-    
-        print('-'*10)
+        cols = row.find_all(['td','th'])
+        states.add(cols[1].get_text())
+
+    for state in list(states):
+        l = []
+        for row in rows:
+            cols = row.find_all(['td', 'th'])          
+            if cols[1].get_text() == state:
+                l.append(cols[0].get_text())
+                #print()
+        d[state] = l
+
+    print(d)
 
 if __name__ == '__main__':
     main()
